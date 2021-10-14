01432f4a5b4006a3ac7a98869a65fdd7abd1300c
Nitish Kesarwani <Nitish.Kesarwani@rsystems.com>
2021-10-14 09:50:50 +0530

    add cd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['images.ctfassets.net'],
-  },
+  assetPrefix: isProd ? 'https://mvhydraendpoint.azureedge.net' : '',
+  // images: {
+  //   domains: ['images.ctfassets.net'],
+  // },
 }
